554ae985a345d822949e75893cc4da18eb4f3500
Mateus Silva <gmateusjose6@gmail.com>
2020-12-27 09:57:54 -0300

    add a index and a professors route

--- application.py
+++ application.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # Define a main route for the application    
+    return render_template('index.html')
+
+
+@app.route('/professors')
+def professors():
+    # Define a route to deal with professors
+    return render_template('professors.html')
