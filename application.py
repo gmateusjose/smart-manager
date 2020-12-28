72288f5d8081e2f3db52b4b4d111e7647ffe4f45
Mateus Silva <gmateusjose6@gmail.com>
2020-12-28 15:55:28 -0300

    rendering the students table

--- application.py
+++ application.py
@@ -16,9 +16,46 @@ def index():
 def students():
     # Define a route to deal with students
     if request.method == 'POST':
-        return render_template('students.html')
+        conn = sqlite3.connect('smart-fluent.db')
+        c = conn.cursor()
+        c.executescript(open('queries/create_tables.sql').read())
+        print(request.form)
+        if 'add-field' in request.form.keys():
+            student = request.form.get('add-field').lower().strip()
+            teacher_id = c.execute('''
+                SELECT id FROM professors WHERE name = ? 
+                ''', (request.form.get('teacher').lower().strip(),))
+            monthly = float(request.form.get('value'))
+
+            # decide if the student is a scholar
+            if 'scholar' in request.form.keys():
+                c.execute('''
+                    INSERT INTO students (name, teacher_id, monthly, scholar)
+                    VALUES (?, ?, ?, TRUE)
+                    ''', (student, teacher_id, monthly))
+            else:
+                c.execute('''
+                    INSERT INTO students (name, teacher_id, monthly) 
+                    VALUES (?, ?, ?)
+                    ''', student, teacher_id, monthly)
+
+        conn.commit()
+        conn.close()
+        return redirect('/students')
     else:
-        return render_template('students.html')
+        conn = sqlite3.connect('smart-fluent.db')
+        c = conn.cursor()
+        c.executescript(open('queries/create_tables.sql').read())
+        
+        professors = [row[0].title() for row in c.execute('''SELECT name FROM
+            professors''')]
+
+        c.execute('''SELECT * FROM students''')
+        students = c.fetchall()
+        
+        conn.close()
+        return render_template('students.html', professors=professors, \
+            students=students)
 
 
 @app.route('/professors', methods=['GET', 'POST'])
