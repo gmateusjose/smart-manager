06b1f56c2210ae23a6fc7c84fedcf18395dbf181
Mateus Silva <gmateusjose6@gmail.com>
2020-12-29 09:44:24 -0300

    Improving the table of students

--- application.py
+++ application.py
@@ -19,26 +19,25 @@ def students():
         conn = sqlite3.connect('smart-fluent.db')
         c = conn.cursor()
         c.executescript(open('queries/create_tables.sql').read())
-        print(request.form)
+
+        # Verify if the form is a adding one
         if 'add-field' in request.form.keys():
             student = request.form.get('add-field').lower().strip()
-            teacher_id = c.execute('''
-                SELECT id FROM professors WHERE name = ? 
+            c.execute('''SELECT id FROM professors WHERE name = ?
                 ''', (request.form.get('teacher').lower().strip(),))
+            teacher_id = c.fetchone()[0]
             monthly = float(request.form.get('value'))
+            scholar = False
 
             # decide if the student is a scholar
             if 'scholar' in request.form.keys():
-                c.execute('''
-                    INSERT INTO students (name, teacher_id, monthly, scholar)
-                    VALUES (?, ?, ?, TRUE)
-                    ''', (student, teacher_id, monthly))
-            else:
-                c.execute('''
-                    INSERT INTO students (name, teacher_id, monthly) 
-                    VALUES (?, ?, ?)
-                    ''', student, teacher_id, monthly)
-
+                scholar = True
+            
+            c.execute('''
+                INSERT INTO students (name, teacher_id, monthly, scholar)
+                VALUES (?, ?, ?, ?)
+                ''', (student, teacher_id, monthly, scholar))
+        
         conn.commit()
         conn.close()
         return redirect('/students')
@@ -49,10 +48,29 @@ def students():
         
         professors = [row[0].title() for row in c.execute('''SELECT name FROM
             professors''')]
-
-        c.execute('''SELECT * FROM students''')
-        students = c.fetchall()
         
+        students = []
+
+        for row in c.execute('''SELECT * FROM students ORDER BY name'''):
+            student = {
+                'id': row[0],
+                'name': row[1].title(),
+                'teacher': row[2],
+                'payment': '${:,.2f}'.format(row[3]),
+                'scholar': ''
+            }
+            # Add the scholar value
+            if row[4] == 1:
+                student['scholar'] = 'scholar'
+
+            # Append the student to list
+            students.append(student)
+        
+        # Get all the professors name for each student
+        for student in students:
+            c.execute('''SELECT name FROM professors WHERE id = ?''', (student['teacher'],))
+            student['teacher'] = c.fetchone()[0].title()
+
         conn.close()
         return render_template('students.html', professors=professors, \
             students=students)
