10c8a2ab04f7e616a68768fd0396378c40eb6094
Mateus Silva <gmateusjose6@gmail.com>
2020-12-30 08:58:55 -0300

    correcting the java script buttons

--- static/students.js
+++ static/students.js
@@ -1,10 +1,21 @@
-let add_student = document.querySelector('#add-field')
+let add_student = document.querySelector('#add-field');
 
 add_student.onkeyup = function() {
   if (add_student.value === '') {
     document.querySelector('#add-button').disabled = true;
   } else {
     document.querySelector('#add-button').disabled = false;
+  }
+}
+
+
+document.querySelector('#search-student').onchange = function (){
+  if (document.querySelector('#search-student').value === ''){
+    document.querySelector('#upd-button').disabled = true;
+    document.querySelector('#rem-button').disabled = true; 
+  } else {
+    document.querySelector('#upd-button').disabled = false;
+    document.querySelector('#rem-button').disabled = false;
   }
 }
 
